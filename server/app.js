fd86908f7015e06158679283da3894fd5850ca64
wanglei158 <1140153859@qq.com>
2020-10-29 15:17:23 +0800

    #f change

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ require('./db/index.js')
 
 const Article = require('./db/model/article')
 
-// 设置后端服务允许跨域请求
+// set cors that response request from client
 app.use(cors({
     origin: '*',
     methods: '*'
@@ -19,7 +19,7 @@ app.get('/search', async (req, res) => {
     const query = req.query;
     let resp = [];
     let keyList = Object.keys(req.query)
-    if (keyList.length === 0) { // 没有查询条件时返回所有数据
+    if (keyList.length === 0) { // no query condition
         resp = await Article.find()
         return res.json(resp)
     }
@@ -28,15 +28,6 @@ app.get('/search', async (req, res) => {
         const reg = new RegExp(req.query[item])
         arr.push({ [item]: {$regex: reg, $options: 'm'}})
     })
-    // let newKeys = keyList.map(item => {
-    //     if (['year', 'month', 'number', 'pages'].includes(item)) {
-    //         return `this.${item} === ${query[item]}`
-    //     }
-    //     return `this.${item}.indexOf("${query[item]}") !== -1`
-    // })
-    // const condition = newKeys.join(' && ')
-    // const resAr = await Article.$where(condition)
-    console.log(arr)
     const resAr = await Article.find({
         $and: arr
     })
--- server/util/config.js
+++ server/util/config.js
@@ -1,5 +1,5 @@
 exports.qiniuConfig = {
     AK: 'ocicYtZZ_0dJYoIh3wCspujV-QTDCL5XD69gmJk7',
     SK: 'b-cKX3xSXwvvgLoZT2UGQNZ-3HCnwxHjZyfIwCGG',
-    expireTime: 7200 * 1000 // 单位ms
+    expireTime: 7200 * 1000 // unit ms
 }
